8241186c5acf9ef3a09c166d47ac02099045bb73
mina <m1nna@naver.com>
2023-01-13 23:41:01 +0900

    [feat] MEMBER 더미 데이터 생성
    1. 이메일 중복 안되도록 더미 데이터 생성
    2. 더미 데이터 json 파일로 변환
    3. json 파일 → bulk insert 적용
    
    Resolves: #11

---
+++ member/insert_fake_member.py
@@ -0,0 +1,30 @@
+import json
+
+from db_connect import cur, conn
+
+i = 1
+
+hundred_thousand = []
+
+while True:
+    try:
+        member_file_path = f'member_{i}.json'
+
+        with open(member_file_path, "r") as member_file:
+            values_data = json.load(member_file)
+            values_data = values_data['member']
+            hundred_thousand.append(values_data)
+
+            if i % 4 == 0:
+                values = str(hundred_thousand).replace("[", "").replace("]", "").replace('"', "")
+
+                sql = "INSERT IGNORE INTO member (EMAIL, PASSWORD, ADDRESS) VALUES" + values
+                cur.execute(sql)
+                conn.commit()
+
+                hundred_thousand = []
+
+        i += 1
+    except:
+        print(f"member_{i}.json 도중 에러")
+        break
