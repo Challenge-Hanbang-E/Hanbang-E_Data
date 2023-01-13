eeb59dd16aee3a19fffd7d53e886b3c5c07effda
mina <m1nna@naver.com>
2023-01-14 00:05:09 +0900

    [feat] MEMBER 더미 데이터 생성
    1. 이메일 중복 안되도록 더미 데이터 생성
    2. 더미 데이터 json 파일로 변환
    3. json 파일 → bulk insert 적용
    
    Resolves: #11

---
+++ member/make_fake_member.py
@@ -0,0 +1,48 @@
+import json
+
+from faker import Faker
+
+fake = Faker('ko-KR')
+
+i = 1
+
+all_email_data = []
+sql_values = []
+email_list = []
+
+variation_value = 30  # 데이터를 더 생성하려면 여기를 변동해주셔야 합니다!
+
+while i <= variation_value:
+    while True:
+        email_file_path = f'data/email/email_{i}.json'
+        member_file_path = f'data/member/member_{i}.json'
+
+        try:
+            with open(email_file_path, "r") as email_file:
+                data = json.load(email_file)
+                email_data = data["email"]
+                all_email_data.append(email_data)
+                i += 1
+        except:
+            email_file_path = f'email_{i}.json'
+            member_file_path = f'member_{i}.json'
+
+            default_email_file = {"email": []}
+            default_member_file = {"member": []}
+            email_list = default_email_file["email"]
+            sql_values = default_member_file["member"]
+            break
+
+    while len(email_list) < 25000:
+        email_split = fake.email().split("@")
+        email_change = email_split[0] = email_split[0] + f"{len(all_email_data)}" + f"{len(email_list)}@"
+        fake_email = "".join(email_split)
+        email_list.append(fake_email)
+        value = f"('{fake_email}', '{fake.password()}', '{fake.address()}')"
+        sql_values.append(value)
+
+    with open(email_file_path, 'w') as email_outfile:
+        json.dump(default_email_file, email_outfile, ensure_ascii=False)
+
+    with open(member_file_path, 'w') as member_outfile:
+        json.dump(default_member_file, member_outfile, indent=1, ensure_ascii=False)
