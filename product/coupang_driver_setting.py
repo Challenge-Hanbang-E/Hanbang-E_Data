70886814889044c45d737173b9dae6780138e23c
mina <m1nna@naver.com>
2023-01-14 18:51:08 +0900

    [refactor] 상품 크롤링
    - full screen 설정 적용 취소
    
    Resolves:  #18

--- product/coupang_driver_setting.py
+++ product/coupang_driver_setting.py
@@ -7,7 +7,7 @@ def driver_connection():
     options = webdriver.ChromeOptions()
     # options.add_argument("headless")  # 화면 사용 X
     options.add_argument('window-size=1920x1080')
-    options.add_argument('--start-fullscreen')
+    # options.add_argument('--start-fullscreen')
     options.add_argument("--incognito")  # 시크릿 모드(쿠키 X)
     options.add_argument("--disable-popup-blocking")  # 광고 팝업 노출 X
 
