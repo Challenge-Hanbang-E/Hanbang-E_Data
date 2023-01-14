bc6013d516f9800877307ecf5005f5fea676d45c
mina <m1nna@naver.com>
2023-01-14 18:48:39 +0900

    [refactor] DB 적재 로직 변경
    하나의 파일이 충분히 클 것을 대비해 하나의 파일씩 insert 하도록 변경
    
    Related to: #18

--- product/insert_product.py
+++ product/insert_product.py
@@ -2,29 +2,21 @@ import json
 
 from db_connect import cur, conn
 
-i = 9  # 고정 값
+i = 1
 j = 1
+last_j = 3
 
-products_data_combine = []
+while j <= last_j:
+    product_file_path = f'./products/product_{i}_{j}.json'
+    with open(product_file_path, "r") as product_file:
+        values_data = json.load(product_file)
+        products = values_data['product']
+        print(len(products))
+        values = str(products).replace("[", "").replace("]", "").replace('"', "")
+        sql = "INSERT INTO product (NAME, PRICE, IMG, STOCK, SALES, ON_SALE) VALUES" + values
+        cur.execute(sql)
+        conn.commit()
 
-while j <= 5:  # j크기: 변동 값
-    try:
-        product_file_path = f'./product/product_{i}_{j}.json'
-
-        with open(product_file_path, "r") as product_file:
-            values_data = json.load(product_file)
-            products = values_data['product']
-            products_data_combine.append(products)
         j += 1
 
-    except:
-        print(f"member_{i}_{j}.json 존재 안함")
-        j += 1
-
-
-values = str(products_data_combine).replace("[", "").replace("]", "").replace('"', "")
-sql = "INSERT INTO product (NAME, PRICE, IMG, STOCK, SALES, ON_SALE) VALUES" + values
-
-cur.execute(sql)
-conn.commit()
 conn.close()
