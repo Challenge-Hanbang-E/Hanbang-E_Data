711157aa8d2481ef51901c42eae1e18174428524
mina <m1nna@naver.com>
2023-01-16 01:38:38 +0900

    [fix] selenium 카테고리 이동시 드문 에러 발생
    Resolves: #26

--- product/coupang_driver_setting.py
+++ product/coupang_driver_setting.py
@@ -7,6 +7,7 @@ def driver_connection():
     options = webdriver.ChromeOptions()
     options.add_argument("headless")  # 화면 사용 X
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.20 Safari/537.36")
+    options.add_argument("window-size=1200x600")
     options.add_argument("--incognito")  # 시크릿 모드(쿠키 X)
     options.add_argument("--disable-popup-blocking")  # 광고 팝업 노출 X
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
