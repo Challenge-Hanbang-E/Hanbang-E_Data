1081a85375f1704ee16301f984de427a155ea7ab
mina <m1nna@naver.com>
2023-01-15 18:55:59 +0900

    [refactor] selenium Background 실행
    Related to: #22

--- product/coupang_driver_setting.py
+++ product/coupang_driver_setting.py
@@ -5,12 +5,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def driver_connection():
     options = webdriver.ChromeOptions()
-    # options.add_argument("headless")  # 화면 사용 X
-    options.add_argument('window-size=1920x1080')
-    # options.add_argument('--start-fullscreen')
+    options.add_argument("headless")  # 화면 사용 X
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.20 Safari/537.36")
     options.add_argument("--incognito")  # 시크릿 모드(쿠키 X)
     options.add_argument("--disable-popup-blocking")  # 광고 팝업 노출 X
-
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     # 크롤링 방지 설정 → undefined로 변경
