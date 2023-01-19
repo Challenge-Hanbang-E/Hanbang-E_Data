ff230e0457c3ca4c9ad7e1499381545728a703c6
mina <m1nna@naver.com>
2023-01-19 17:15:08 +0900

    [refactor] Json 파일 db 적제 로직 변경
    Resolves: #30

--- product/insert_product.py
+++ product/insert_product.py
@@ -5,19 +5,25 @@ from logger_config import logger
 
 i = 1
 j = 1
-last_j = 3
 
-while j <= last_j:
-    product_file_path = f'./products/product_{i}_{j}.json'
-    with open(product_file_path, "r") as product_file:
-        values_data = json.load(product_file)
-        products = values_data['product']
-        logger.info(f"product_{i}_{j}.json: {len(products)}")
-        values = str(products).replace("[", "").replace("]", "").replace('"', "")
-        sql = "INSERT INTO product (NAME, PRICE, IMG, STOCK, SALES, ON_SALE) VALUES" + values
-        cur.execute(sql)
-        conn.commit()
+while i <= 15:
+    try:
+        product_file_path = f'./data/datas/product_{i}_{j}.json'
+        with open(product_file_path, "r") as product_file:
+            values_data = json.load(product_file)
+            products = values_data['product']
+            logger.info(f"product_{i}_{j}.json: {len(products)}")
+            values = str(products).replace("[", "").replace("]", "").replace('"', "")
+            sql = "INSERT INTO product (NAME, PRICE, IMG, STOCK, SALES, ON_SALE) VALUES" + values
+            cur.execute(sql)
+            conn.commit()
 
+            j += 1
+    except:
+        logger.info(f"product_{i}_{j}.json 존재 안함")
         j += 1
+        if j > 17:
+            i += 1
+            j = 1
 
 conn.close()
