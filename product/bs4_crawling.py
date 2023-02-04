b9c091e5fac0b137f60b171a91b8045221a91b8e
mina <m1nna@naver.com>
2023-02-04 16:54:09 +0900

    [feat] 신상품 크롤링 로직 추가
    Resolves: #32

--- product/bs4_crawling.py
+++ product/bs4_crawling.py
@@ -1,12 +1,16 @@
 import json
 import random
+from datetime import datetime
+
 from bs4 import BeautifulSoup
 
 from logger_config import logger
 
 
 def bs4_crawling(driver, i, j, k, page):
-    file_path = f'./products/product_{i}_{j}.json'
+
+    today = datetime.today().strftime("%Y-%m-%d")
+    file_path = f'./new_products/{today}.json'
     try:
         with open(file_path, "r") as json_file:
             data = json.load(json_file)
@@ -28,19 +32,18 @@ def bs4_crawling(driver, i, j, k, page):
     with open(file_path, 'w') as outfile:
         for element in search_result:
             count += 1
-            sales = random.randrange(1, 10000)
+            # sales = random.randrange(1, 100)
             try:
                 e = element.select_one('a.baby-product-link > dl.baby-product-wrap')
                 img = "https://" + e.select_one(' dt.image > img')['src']
                 product_name = e.select_one('dd > div.name').text.strip().replace('"', "").replace("'", "")
                 price = e.select_one('dd > div > div > div > em > strong').text.replace(",", "")
 
-                json_values = f"('{product_name}', {price}, '{img}', {1000000}, {sales}, {True})"
+                json_values = f"('{product_name}', {price}, '{img}', {70}, {0}, {True})"
                 data['product'].append(json_values)
 
             except:
                 logger.info(f"실패: 카테고리: {i} > {j} > {k}, 페이지: {page}, 카운트: {count}")
-
 
         json.dump(data, outfile, indent=1, ensure_ascii=False)
 
